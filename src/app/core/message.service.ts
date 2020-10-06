39c232d1a051d69778ae9e1537bb09992fca2a4d
Diego Mota <diegomota@outlook.com>
2020-10-05 21:49:35 -0300

    Implementado o Serviço de mensagem.

--- src/app/core/message.service.ts
+++ src/app/core/message.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  constructor(private toastrService: ToastrService) { }
+
+  showSucess(titulo, mensagem){
+    this.toastrService.success(titulo, mensagem);
+  }
+
+  showWarning(titulo, mensagem){
+    this.toastrService.warning(titulo, mensagem);
+  }
+
+  showError(titulo, mensagem){
+    this.toastrService.error(titulo, mensagem);
+  }
+  
 }
